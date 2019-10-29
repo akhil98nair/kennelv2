0fb7db0906d23a57df14837c50f326e8c40953ed
akhil <akhil.nair16@siesgst.ac.in>
2019-10-29 19:08:20 +0530

    push

--- app/src/main/java/com/miniproject/kennelv2/fill_details.java
+++ app/src/main/java/com/miniproject/kennelv2/fill_details.java
@@ -1,14 +1,31 @@
 package com.miniproject.kennelv2;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +35,13 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -27,37 +49,68 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class fill_details extends AppCompatActivity {
-    EditText name, mobile, location, email, comment;
+    EditText name, mobile, location1, email, comment;
+    String full_path, fileUri, rand;
     ImageView imageView;
-    Button camera,submit_button, getlocation;
+    Button camera,submit_button, getlocation, fetchlocation, take_image;
+    DatabaseReference myRef;
     FirebaseStorage storage;
-    StorageReference storageReference;
+    StorageReference storageReference, ref;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private Uri filePath;
-
+    FirebaseAuth mAuth;
+    LocationManager locationManager;
+    static final int REQUEST_PICTURE_CAPTURE = 1;
+    
 
 
     private final int PICK_IMAGE_REQUEST = 71;
+    private String pictureFilePath;
+    private String deviceIdentifier;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_details);
+        take_image = (Button) findViewById(R.id.upload);
+        fetchlocation = (Button) findViewById(R.id.showAddress);
         camera = (Button) findViewById(R.id.camerabutton);
         name = (EditText)findViewById(R.id.name);
         comment = (EditText) findViewById(R.id.comment);
         mobile = (EditText)findViewById(R.id.number);
-        location = (EditText)findViewById(R.id.location);
+        location1 = (EditText)findViewById(R.id.location);
         email = (EditText)findViewById(R.id.email);
         submit_button = (Button) findViewById(R.id.submit);
         imageView = (ImageView) findViewById(R.id.dogimage);
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+
+            /* perform your actions here*/
+
+
+        } else {
+            signInAnonymously();
+        }
+
+
+        getInstallationIdentifier();
+
+
+
 
         getlocation = (Button) findViewById(R.id.getloc);
         getlocation.setOnClickListener(new View.OnClickListener() {
@@ -77,10 +130,10 @@ public class fill_details extends AppCompatActivity {
             public void onClick(View view) {
                 try
                 {
-                    DatabaseReference myRef = database.getReference("details").push();
+                    myRef = database.getReference("details").push();
                     String user_name = name.getText().toString();
                     String mob_no = mobile.getText().toString();
-                    String loc = location.getText().toString();
+                    String loc = location1.getText().toString();
                     String email_id = email.getText().toString();
                     String get_comment = comment.getText().toString();
                     myRef.child("name").setValue(user_name);
@@ -88,10 +141,13 @@ public class fill_details extends AppCompatActivity {
                     myRef.child("comment").setValue(get_comment);
                     myRef.child("loc").setValue(loc);
                     myRef.child("email").setValue(email_id);
-                    myRef.child("image_path").setValue(uploadImage());
+
+                    uploadImage();
+
+//
                     name.setText("");
                     mobile.setText("");
-                    location.setText("");
+                    location1.setText("");
                     email.setText("");
                     comment.setText("");
                     imageView.setImageBitmap(null);
@@ -120,7 +176,72 @@ public class fill_details extends AppCompatActivity {
             }
         });
 
+        take_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try
+                {
+                    sendTakePictureIntent();
+                }
+                catch (Exception e)
+                {
+                    Log.d(TAG, "onClick");
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+    private void sendTakePictureIntent() {
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra( MediaStore.EXTRA_FINISH_ON_COMPLETION, true);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, REQUEST_PICTURE_CAPTURE);
+
+            File pictureFile = null;
+            try {
+                pictureFile = getPictureFile();
+            } catch (IOException ex) {
+                Toast.makeText(this,
+                        "Photo file can't be created, please try again",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (pictureFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.miniproject.kennelv2.fill_details",
+                        pictureFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(cameraIntent, REQUEST_PICTURE_CAPTURE);
+            }
+        }
+    }
+    private File getPictureFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String pictureFile = "ZOFTINO_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(pictureFile,  ".jpg", storageDir);
+        pictureFilePath = image.getAbsolutePath();
+        return image;
+    }
+
+    protected synchronized String getInstallationIdentifier() {
+        if (deviceIdentifier == null) {
+            SharedPreferences sharedPrefs = this.getSharedPreferences(
+                    "DEVICE_ID", Context.MODE_PRIVATE);
+            deviceIdentifier = sharedPrefs.getString("DEVICE_ID", null);
+            if (deviceIdentifier == null) {
+                deviceIdentifier = UUID.randomUUID().toString();
+                SharedPreferences.Editor editor = sharedPrefs.edit();
+                editor.putString("DEVICE_ID", deviceIdentifier);
+                editor.commit();
+            }
+        }
+        return deviceIdentifier;
+    }
+
+
     private void chooseImage() {
         Intent intent = new Intent();
         intent.setType("image/*");
@@ -128,25 +249,107 @@ public class fill_details extends AppCompatActivity {
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
     }
 
+    private void signInAnonymously() {
+        mAuth.signInAnonymously().addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                /* perform your actions here*/
+
+            }
+        })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.e("MainActivity", "signFailed****** ", exception);
+                    }
+                });
+    }
+
+
     private String uploadImage() {
-        String rand = UUID.randomUUID().toString();
+        rand = UUID.randomUUID().toString();
+        ref = storageReference.child(rand);
         if(filePath != null)
         {
             final ProgressDialog progressDialog = new ProgressDialog(this);
             progressDialog.setTitle("Uploading...");
             progressDialog.show();
 
-            StorageReference ref = storageReference.child("images/"+ rand);
+
             ref.putFile(filePath)
                     .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                             progressDialog.dismiss();
                             Toast.makeText(fill_details.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                           ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    myRef.child("image_path").setValue(uri.toString());
+                                    // Got the download URL for 'users/me/profile.png'
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle any errors
+                                }
+                            });
 
 
                         }
                     })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(fill_details.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+
+        }
+
+        else if(pictureFilePath != null)
+        {
+            File f = new File(pictureFilePath);
+            Uri picUri = Uri.fromFile(f);
+            final String cloudFilePath = deviceIdentifier + picUri.getLastPathSegment();
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+
+            ref.putFile(picUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(fill_details.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    myRef.child("image_path").setValue(uri.toString());
+                                    // Got the download URL for 'users/me/profile.png'
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle any errors
+                                }
+                            });
+
+
+                        }
+                    })
+
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
@@ -187,7 +390,19 @@ public class fill_details extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+
+        else if (requestCode == REQUEST_PICTURE_CAPTURE && resultCode == RESULT_OK) {
+            File imgFile = new  File(pictureFilePath);
+            if(imgFile.exists())            {
+                imageView.setImageURI(Uri.fromFile(imgFile));
+            }
+        }
     }
 
+
 }
+
+
+
+
 
